063e9d52ce7518cacf093675fe17e934b9ce1719
山本恭平 <k_yamamoto@bs-ja.co.jp>
2020-10-14 18:04:13 +0900

    編集画面のバグ修正

--- Archive/Edit.cs
+++ Archive/Edit.cs
@@ -28,7 +28,8 @@ namespace Archive
         private void editButton_Click(object sender, EventArgs e)
         {
             //DBに接続する処理
-            string sLogin = "server=192.168.8.102; database=books; userid=bks; password=bksbooklist;";
+            //string sLogin = "server=192.168.8.102; database=books; userid=bks; password=bksbooklist;";
+            string sLogin = "server=localhost; database=books; userid=root; password=Yamakyo0811@;";
 
             MySqlConnection cn = new MySqlConnection(sLogin);
             DataTable dt = new DataTable();
@@ -38,45 +39,59 @@ namespace Archive
             //申請する書籍のIDを一時格納
             string[] bookIdList = new string[editGridView.Rows.Count];
 
+            string bookname = "";
+
             for (int i = 0; i < editGridView.Rows.Count; i++)
             {
                 bookIdList[i] = (string)editGridView.Rows[i].Cells[1].Value;
+                bookname = (string)editGridView.Rows[i].Cells[2].Value;
 
-                //貸出開始日と貸出終了日に日付が入っている(=貸出中 or 期限切れ)場合はすべて更新
-                if ((!string.IsNullOrEmpty((editGridView.Rows[i].Cells[3].Value).ToString())) && (!string.IsNullOrEmpty((editGridView.Rows[i].Cells[4].Value).ToString())))
+                //書名が入力されている場合は処理開始
+                if (!string.IsNullOrEmpty(bookname))
                 {
-                    sql = "UPDATE books.books " +
-                          "SET BOOK_NAME = '" + editGridView.Rows[i].Cells[2].Value + "' , " +
-                          "LOAN_DATE = '" + editGridView.Rows[i].Cells[3].Value + "' , " +
-                          "RETURN_DATE = '" + editGridView.Rows[i].Cells[4].Value + "' " +
-                          "WHERE BOOK_ID = '" + bookIdList[i] + "' ";
+                    //貸出日と返却期日に日付が入っている(=貸出中 or 期限切れ)場合はすべて更新
+                    if ((!string.IsNullOrEmpty((editGridView.Rows[i].Cells[3].Value).ToString())) && (!string.IsNullOrEmpty((editGridView.Rows[i].Cells[4].Value).ToString())))
+                    {
+                        sql = "UPDATE books.books " +
+                              "SET BOOK_NAME = '" + editGridView.Rows[i].Cells[2].Value + "' , " +
+                              "LOAN_DATE = '" + editGridView.Rows[i].Cells[3].Value + "' , " +
+                              "RETURN_DATE = '" + editGridView.Rows[i].Cells[4].Value + "' " +
+                              "WHERE BOOK_ID = '" + bookIdList[i] + "' ";
+                    }
+                    //貸出日と返却期日に日付が入っていない(=貸出されていない)場合は書籍名のみ更新
+                    else if ((string.IsNullOrEmpty((editGridView.Rows[i].Cells[3].Value).ToString())) && (string.IsNullOrEmpty((editGridView.Rows[i].Cells[4].Value).ToString())))
+                    {
+                        sql = "UPDATE books.books " +
+                              "SET BOOK_NAME = '" + editGridView.Rows[i].Cells[2].Value + "' " +
+                              "WHERE BOOK_ID = '" + bookIdList[i] + "' ";
+                    }
+
+                    //SQL文実行
+                    MySqlDataAdapter da = new MySqlDataAdapter(sql, cn);
+
+                    try
+                    {
+                        cn.Open();
+
+                        da.Fill(dt);
+
+                        MessageBox.Show("更新完了");
+
+                        //DBとの接続をcloseする
+                        cn.Close();
+                    }
+                    catch (MySqlException me)
+                    {
+                        MessageBox.Show("ERROR: " + me.Message);
+                    }
                 }
-                //貸出開始日と貸出終了日に日付が入っていない(=貸出されていない)場合は書籍名のみ更新
                 else
                 {
-                    sql = "UPDATE books.books " +
-                          "SET BOOK_NAME = '" + editGridView.Rows[i].Cells[2].Value + "' " +
-                          "WHERE BOOK_ID = '" + bookIdList[i] + "' ";
-                }
-
-                //SQL文実行
-                MySqlDataAdapter da = new MySqlDataAdapter(sql, cn);
-
-                try
-                {
-                    cn.Open();
-
-                    da.Fill(dt);
-
-                    //DBとの接続をcloseする
-                    cn.Close();
-                }
-                catch (MySqlException me)
-                {
-                    MessageBox.Show("ERROR: " + me.Message);
+                    MessageBox.Show("ERROR: 書名が未入力です。");
+                    break;
                 }
             }
-            MessageBox.Show("更新完了");
+
         }
     }
 }
